3f5790a3e2f5c39a837accaa76803361419c20c8
大庭秀斗 <shuto.0028.0618@gmail.com>
2022-12-20 23:51:16 +0900

    add sample form

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/rivo/tview"
+)
+
+func main() {
+	app := tview.NewApplication()
+	form := tview.NewForm().
+		AddInputField("Title", "", 20, nil, nil).
+		AddDropDown("Importance", []string{"1", "2", "3", "4", "5"}, 0, nil).
+		AddTextArea("detail", "", 40, 0, 0, nil).
+		AddCheckbox("Done", false, nil).
+		AddButton("Save", nil).
+		AddButton("Quit", func() {
+			app.Stop()
+		})
+	if err := app.SetRoot(form, true).EnableMouse(true).Run(); err != nil {
+		panic(err)
+	}
+}
